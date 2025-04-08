573a701406e55a5f1841773893c1ea29ae9686e2
FranklinLMR <rosariosanchezfranklin@gmail.com>
2025-04-08 07:57:56 -0400

    New changes

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import flet as ft
+import os
 
+def done():
+    os.remove("Files")        
 def main(page: ft.Page):
     #This is just a comment
-    page.add(ft.Text("Hola"), ft.ElevatedButton("Press Here"))
+    page.add(ft.Text("Hola"), ft.ElevatedButton("Press Here", on_click=done))
     
 
 ft.app(main)
